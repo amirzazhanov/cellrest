78fa049bbc00503181d005e2b6ddff784060c135
Adel Mirzazhanov <adel.mirzazhanov@gmail.com>
2017-05-31 16:48:33 +0600

    cell struct added

--- cellrest.go
+++ cellrest.go
@@ -8,8 +8,34 @@ import (
 	"net/http"
 )
 
+/*
+radio,mcc,net,area,cell,unit,lon,lat,range,samples,changeable,created,updated,averageSignal
+UMTS,262,2,801,86355,,13.28527,52.521711,37,7,1,1282569574,1300175362,-91
+GSM,262,2,801,1795,,13.276605,52.525348,5714,9,1,1282569574,1300175362,-87
+GSM,262,2,801,1794,,13.284916,52.523771,6278,13,1,1282569574,1300816026,-91
+UMTS,262,2,801,211250,,13.285081,52.521622,33,3,1,1282569574,1299486792,-94
+*/
+type Cell struct {
+	Radio         string
+	MCC           uint32
+	Net           uint32
+	Area          uint32
+	Cell          uint32
+	Unit          uint32
+	Lon           float32
+	Lat           float32
+	Range         uint32
+	Samples       uint32
+	Changeable    bool
+	Created       uint32
+	Updated       uint32
+	averageSignal uint32
+}
+
+type Cells []Cell
+
 func main() {
-	r := mux.NewRouter() //.StrictSlash(true)
+	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", Index)
 	r.HandleFunc("/cells", CellIndex)
 	r.HandleFunc("/cells/{cellId}", CellShow)
