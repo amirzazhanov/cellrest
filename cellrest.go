cb4bf5e76da2da0a9f77000f7b45fcb49df77d73
Adel Mirzazhanov <adel.mirzazhanov@gmail.com>
2017-05-31 14:39:04 +0600

    router and some basic routes has been added

--- cellrest.go
+++ cellrest.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
-	"html"
+	"github.com/gorilla/mux"
+	//	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	r := mux.NewRouter() //.StrictSlash(true)
+	r.HandleFunc("/", Index)
+	r.HandleFunc("/cells", CellIndex)
+	r.HandleFunc("/cells/{cellId}", CellShow)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
+
+// Index function
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+// CellIndex - list all cells
+func CellIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
+}
+
+//CellShow - show specific cell
+func CellShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	cellID := vars["cellId"]
+	fmt.Fprintln(w, "Cell show:", cellID)
 }
